c44239ab0aed173c46e312e6192279308db59e45
Cristobal Rincon <57472953+terracris@users.noreply.github.com>
2024-06-14 13:11:22 -0400

    Update arm.py
    dumped trajectory motion stuff from modern robotics. need to integrate into arm class.

--- arm.py
+++ arm.py
@@ -123,7 +123,81 @@ class Arm:
         N = 5      # number of points in trajectory
         method = 5 # time-scaling method (quintic)
         theta_start = self.get_current_theta()
-        #joint_traj = mr.JointTrajectory(theta_start, ik, tf, N, method)
+        def CubicTimeScaling(Tf, t):
+    """Computes s(t) for a cubic time scaling
+
+    :param Tf: Total time of the motion in seconds from rest to rest
+    :param t: The current time t satisfying 0 < t < Tf
+    :return: The path parameter s(t) corresponding to a third-order
+             polynomial motion that begins and ends at zero velocity
+
+    Example Input:
+        Tf = 2
+        t = 0.6
+    Output:
+        0.216
+    """
+    return 3 * (1.0 * t / Tf) ** 2 - 2 * (1.0 * t / Tf) ** 3
+
+def QuinticTimeScaling(Tf, t):
+    """Computes s(t) for a quintic time scaling
+
+    :param Tf: Total time of the motion in seconds from rest to rest
+    :param t: The current time t satisfying 0 < t < Tf
+    :return: The path parameter s(t) corresponding to a fifth-order
+             polynomial motion that begins and ends at zero velocity and zero
+             acceleration
+
+    Example Input:
+        Tf = 2
+        t = 0.6
+    Output:
+        0.16308
+    """
+    return 10 * (1.0 * t / Tf) ** 3 - 15 * (1.0 * t / Tf) ** 4 \
+           + 6 * (1.0 * t / Tf) ** 5
+
+def JointTrajectory(thetastart, thetaend, Tf, N, method):
+    """Computes a straight-line trajectory in joint space
+
+    :param thetastart: The initial joint variables
+    :param thetaend: The final joint variables
+    :param Tf: Total time of the motion in seconds from rest to rest
+    :param N: The number of points N > 1 (Start and stop) in the discrete
+              representation of the trajectory
+    :param method: The time-scaling method, where 3 indicates cubic (third-
+                   order polynomial) time scaling and 5 indicates quintic
+                   (fifth-order polynomial) time scaling
+    :return: A trajectory as an N x n matrix, where each row is an n-vector
+             of joint variables at an instant in time. The first row is
+             thetastart and the Nth row is thetaend . The elapsed time
+             between each row is Tf / (N - 1)
+
+    Example Input:
+        thetastart = np.array([1, 0, 0, 1, 1, 0.2, 0,1])
+        thetaend = np.array([1.2, 0.5, 0.6, 1.1, 2, 2, 0.9, 1])
+        Tf = 4
+        N = 6
+        method = 3
+    Output:
+        np.array([[     1,     0,      0,      1,     1,    0.2,      0, 1]
+                  [1.0208, 0.052, 0.0624, 1.0104, 1.104, 0.3872, 0.0936, 1]
+                  [1.0704, 0.176, 0.2112, 1.0352, 1.352, 0.8336, 0.3168, 1]
+                  [1.1296, 0.324, 0.3888, 1.0648, 1.648, 1.3664, 0.5832, 1]
+                  [1.1792, 0.448, 0.5376, 1.0896, 1.896, 1.8128, 0.8064, 1]
+                  [   1.2,   0.5,    0.6,    1.1,     2,      2,    0.9, 1]])
+    """
+    N = int(N)
+    timegap = Tf / (N - 1.0)
+    traj = np.zeros((len(thetastart), N))
+    for i in range(N):
+        if method == 3:
+            s = CubicTimeScaling(Tf, timegap * i)
+        else:
+            s = QuinticTimeScaling(Tf, timegap * i)
+        traj[:, i] = s * np.array(thetaend) + (1 - s) * np.array(thetastart)
+    traj = np.array(traj).T
+    return traj
         return None
     
     def follow_trajectory(self, trajecory):
